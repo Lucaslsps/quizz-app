a5e49e58ea6e138b6e779979436fc7b3f5ea60d8
Lucas Peixoto <lucas.lsps@hotmail.com>
2020-03-20 01:02:29 -0300

    Front básico
    MVP do front implementado

--- public/js/select_option.js
+++ public/js/select_option.js
@@ -1,5 +1,62 @@
-console.log("blalb");
+//Send post
+//$( "#yourFormId" ).submit();
 
-function selecionado(){
-    console.log("primeiro");
+var img1_value = 0;
+var img2_value = 0;
+var img3_value = 0;
+
+function define_color(card, img_value){
+    card_sel = document.getElementsByClassName(card);
+    switch (img_value){
+        case 1:
+            card_sel[0].style.background = "#DAA520";
+            break;
+        case 2:
+            card_sel[0].style.background = "#BCBDB3";
+            break;
+        case 3:
+            card_sel[0].style.background = "#836953";        
+            break;
+    }
+    
 }
+
+$(document).ready(function(){
+    $( "#img1" ).click(function() {
+        if(img1_value <=2){
+            img1_value += 1;
+        }else{
+            img1_value = 1;
+        }
+        define_color("sel1", img1_value);
+    });
+
+    $( "#img2" ).click(function() {
+        if(img2_value <=2){
+            img2_value += 1;
+        }else{
+            img2_value = 1;
+        }
+        define_color("sel2", img2_value);
+    });
+    
+    $( "#img3" ).click(function() {
+        if(img3_value <=2){
+            img3_value += 1;
+        }else{
+            img3_value = 1;
+        }
+        define_color("sel3", img3_value);
+    });
+
+    $("#enviar").click(function(){
+        var img_values = [img1_value, img2_value, img3_value];
+        $.ajax({
+            type:'POST',
+            url:'/',
+            data: img_values,
+            success:function(reponse){console.log("foi")}
+        });
+    });
+});
+
--- server.js
+++ server.js
@@ -3,22 +3,23 @@ const bodyParser = require('body-parser');
 const request = require('request');
 const app = express()
 
-
+const PORT = process.env.PORT || 3000;
 num = 0;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
   res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
+  console.log(req.body.img_values);
   console.log("postado" + num);
   num++;
 })
 
-app.listen(process.env.PORT || 3100, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(PORT, function () {
+  console.log(`Servidor ouvindo na porta ${PORT}`);
 })
